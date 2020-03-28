855794c6240ad23b3bfc06962b02415784fe8004
Hikar1149 <122094854@qq.com>
2020-03-28 22:43:00 +0800

    feat: logout endpoints

--- src/index.js
+++ src/index.js
@@ -24,16 +24,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
 });
-
-// const jwt = require("jsonwebtoken");
-
-// const myFunction = async () => {
-//   const token = jwt.sign({ _id: "abc777" }, "thismynewcourse", {
-//     expiresIn: "7 days"
-//   });
-//   console.log({ token });
-
-//   const data = jwt.verify(token, "thismynewcourse");
-//   console.log({ data });
-// };
-// myFunction();
