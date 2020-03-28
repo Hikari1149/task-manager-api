c28ff0296cddb26f9d0eed3aa94c3ec593f48107
Hikar1149 <122094854@qq.com>
2020-03-28 21:32:09 +0800

    feat: auth middleware

--- src/index.js
+++ src/index.js
@@ -1,26 +1,39 @@
-const express = require('express')
-require('./db/mongoose') //connect to databse
-const userRouter = require('./routers/user')
-const taskRouter = require('./routers/task')
-const app = express()
-const port = process.env.PORT || 3000
+const express = require("express");
+require("./db/mongoose"); //connect to databse
+const userRouter = require("./routers/user");
+const taskRouter = require("./routers/task");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.use(express.json())
-app.use(userRouter)
-app.use(taskRouter)
+// app.use((req, res, next) => {
+//   if (req.method === "GET") {
+//     res.send("Get requests are disabled");
+//   } else {
+//     next();
+//   }
+// });
 
-app.listen(port,()=>{
-    console.log(`Server is up on port ${port}.`)
-})
+// app.use((req, res, next) => {
+//   res.status(503).send("Under maintenance");
+// });
 
-const jwt = require('jsonwebtoken')
+app.use(express.json());
+app.use(userRouter);
+app.use(taskRouter);
 
-const myFunction = async()=>{
-    const token = jwt.sign({_id:'abc777' },'thismynewcourse',{expiresIn:'7 days'})
-    console.log({token})
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}.`);
+});
 
-    const data = jwt.verify(token,'thismynewcourse')
-    console.log({data})
-     
-} 
-myFunction()
+// const jwt = require("jsonwebtoken");
+
+// const myFunction = async () => {
+//   const token = jwt.sign({ _id: "abc777" }, "thismynewcourse", {
+//     expiresIn: "7 days"
+//   });
+//   console.log({ token });
+
+//   const data = jwt.verify(token, "thismynewcourse");
+//   console.log({ data });
+// };
+// myFunction();
