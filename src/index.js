56e209fb3b15db26e92bff835fa9d870fcc2b8dc
Hikar1149 <122094854@qq.com>
2020-04-05 19:40:31 +0800

    feat: use multer to handle buffer avatar

--- src/index.js
+++ src/index.js
@@ -5,17 +5,30 @@ const taskRouter = require("./routers/task");
 const app = express();
 const port = process.env.PORT || 3000;
 
-// app.use((req, res, next) => {
-//   if (req.method === "GET") {
-//     res.send("Get requests are disabled");
-//   } else {
-//     next();
-//   }
-// });
-
-// app.use((req, res, next) => {
-//   res.status(503).send("Under maintenance");
-// });
+//
+const multer = require("multer");
+const upload = multer({
+  dest: "imgs",
+  limits: {
+    fileSize: 1 * 1024 * 1024
+  },
+  fileFilter(req, file, cb) {
+    if (!file.originalname.match(/\.(doc|docx)$/)) {
+      return cb(new Error("Please  upload a Word doc"));
+    }
+    cb(undefined, true);
+  }
+});
+app.post(
+  "/upload",
+  upload.single("upload"),
+  (req, res) => {
+    res.send("");
+  },
+  (error, req, res, next) => {
+    res.status(400).send({ error: error.message });
+  }
+);
 
 app.use(express.json());
 app.use(userRouter);
@@ -24,16 +37,3 @@ app.use(taskRouter);
 app.listen(port, () => {
   console.log(`Server is up on port ${port}.`);
 });
-
-const Task = require("./models/task");
-const User = require("./models/user");
-
-const main = async () => {
-  // const task = await Task.findById(`5e86027103f0fd09cb8f274e`);
-  // await task.populate("owner").execPopulate();
-  // console.log(task.owner);
-  // const user = await User.findById("5e86010aa2ea250945690f98");
-  // await user.populate("tasks").execPopulate();
-  // console.log(user.tasks);
-};
-main();
